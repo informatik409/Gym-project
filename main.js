ac6fd0934d1e4199ec99b8f7e76e8222197117ff
al1mov <al1movdev.2008@gmail.com>
2022-05-27 14:13:52 +0500

    text qo'shildi

---
+++ main.js
@@ -0,0 +1,10 @@
+let a = document.querySelector(".sign");
+document.getElementById('btn').onclick = function(){
+    a.classList.add("show");
+    a.classList.remove("hide");
+}
+// let b = document.querySelector(".sign");
+// document.getElementById('close').onclick = function(){
+//     b.classList.add("hide");
+//     b.classList.remove("show");
+// }
